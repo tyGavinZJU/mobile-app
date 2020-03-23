843ca979afc13bc9ec663896b485ffb8ee0f4c02
Gavin <gavin@blockstack.com>
2020-03-23 13:13:32 +0800

    adjust folder stucture

---
+++ README.md
@@ -0,0 +1,9 @@
+## 前端文件抽离
+
+- src/components 组件
+- src/views 页面
+- src/common 一些常识性的函数，变量、常量
+- src/theme 管理全局统一的一些样式
+
+views文件夹管理页面 通过router来链接
+components文件夹管理组件 通过views来链接
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View,StyleSheet } from 'react-native';
+import { ListItem, Header,Card,Button,Text,Icon,Input} from 'react-native-elements'
+
+export default class App  extends Component {
+  render() {
+    return (
+      <View>
+        <Header
+          placement="center"
+          statusBarProps={{ barStyle: 'light-content' }}
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: '留言板', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+          containerStyle={{
+            backgroundColor: '#3D6DCC',
+            justifyContent: 'space-around',
+          }}
+          statusBarProps={{
+            translucent: true,
+          }}
+        />
+        <Card
+          title='添加事务'
+          containerStyle={{borderRadius: 2}}
+          >
+          <View>
+            <Input
+              placeholder='内容'
+              containerStyle={{width:100}}
+            />
+            <Button
+              icon={<Icon name='code' color='#ffffff' />}
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0,backgroundColor: '#3D6DCC',width:100}}
+              
+              title='提交' />
+          </View>
+
+
+        </Card>
+
+        <Card
+          title='待办事务'
+          containerStyle={{borderRadius: 15}}
+          >
+
+          <Text style={{marginBottom: 10,marginTop: 10}}>
+            The idea with React Native Elements is more about component structure than actual design.
+          </Text>
+        </Card>
+      </View>
+    );
+  }
+}
+
+
